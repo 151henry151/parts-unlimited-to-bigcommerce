d652ecd95d9c5e77920f9c99972c177d8eff0a9d
Henry <henry@debian>
2015-05-19 21:04:36 +0000

    trimming it down to help figure it out. don't forget to wrap it in a function again later.

--- python_scripts/pull_from_database.py
+++ python_scripts/pull_from_database.py
@@ -1,26 +1,24 @@
+#!/usr/bin/python
 import mysql.connector
-from mysql.connector import MySQL Connection, Error
+from mysql.connector import Error
 import getpass 
 
-
-
-def get_part_info(pn):
-    mysqlpasswd = getpass.getpass('Mysql root password:')
-    try:
-        conn = mysql.connector.connect(host='localhost',
-                                       database='PARTDATA',
-                                       user='root',
-                                       password=mysqlpasswd)
-        cursor = conn.cursor()
-        if conn.is_connected():
-            print('Connected to MySQL database')
-            cursor.execute("SELECT * FROM CatalogContentExport WHERE partNumber=%s", (pn,))
+mysqlpasswd = getpass.getpass('Mysql root password:')
+try:
+    conn = mysql.connector.connect(host='localhost',
+        database='PARTDATA',
+        user='root',
+        password=mysqlpasswd)
+    cursor = conn.cursor()
+    if conn.is_connected():
+        print('Connected to MySQL database')
+        cursor.execute("SELECT * FROM CatalogContentExport WHERE partNumber=%s", (pn,))
+        row = cursor.fetchone()
+        while row is not None:
+            print(row)
             row = cursor.fetchone()
-            while row is not None:
-                print(row)
-                row = cursor.fetchone()
-    except Error as e:
-        print(e)
-    finally:
-        cursor.close()
-        conn.close()
+except Error as e:
+    print(e)
+finally:
+    cursor.close()
+    conn.close()
